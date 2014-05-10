83c0fc4886c85529ca1c13e703087a68547cb8b7
Dave Hall <daveghall@gmail.com>
2014-05-10 16:00:14 -0400

    push

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Create a popcorn instance by calling Popcorn("#id-of-my-video")
+  var pop = Popcorn("#flag-waving");
+
+  // add a footnote at 2 seconds, and remove it at 6 seconds
+  pop.footnote({
+   start: 2,
+   end: 6,
+   text: "Pop!",
+   target: "caption"
+  });
+
+  // play the video right away
+  pop.play();
+}, false);
+
 var myApp = angular.module('myApp',[]);
 myApp.factory('UserService', function() {
   var userService = {};
@@ -23,18 +39,3 @@ function MyCtrl($scope, UserService) {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Create a popcorn instance by calling Popcorn("#id-of-my-video")
-  var pop = Popcorn("#flag-waving");
-
-  // add a footnote at 2 seconds, and remove it at 6 seconds
-  pop.footnote({
-   start: 2,
-   end: 6,
-   text: "Pop!",
-   target: "footnotediv"
-  });
-
-  // play the video right away
-  pop.play();
-}, false);
