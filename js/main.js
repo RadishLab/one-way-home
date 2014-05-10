69cde2e74a33c1fb301da3f5f6bda1e52f681d89
Dave Hall <daveghall@gmail.com>
2014-05-10 13:35:55 -0400

    UGH

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
+var myApp = angular.module('myApp',[]);
+myApp.factory('UserService', function() {
+  var userService = {};
 
+  userService.name = "Hello, Darling";
+
+  userService.ChangeName = function (value) {
+
+      userService.name = value;
+  };
+
+return userService;
 });
+
+function MyCtrl($scope, UserService) {
+    $scope.name = UserService.name;
+    $scope.updatedname="";
+    $scope.changeName=function(data){
+        $scope.updateServiceName(data);
+    }
+    $scope.updateServiceName = function(name){
+      UserService.ChangeName(name);
+      $scope.name = UserService.name;
+    }
+}
