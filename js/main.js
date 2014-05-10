e09ebf3794d4193e1d4832bce34564e80d29ab9c
Dave Hall <daveghall@gmail.com>
2014-05-10 15:14:09 -0400

    Updated

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ myApp.factory('UserService', function() {
   userService.name = "Hello, Darling";
 
   userService.ChangeName = function (value) {
-
-      userService.name = value;
+    userService.name = value;
   };
 
 return userService;
@@ -23,3 +22,19 @@ function MyCtrl($scope, UserService) {
       $scope.name = UserService.name;
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Create a popcorn instance by calling Popcorn("#id-of-my-video")
+  var pop = Popcorn("#flag-waving");
+
+  // add a footnote at 2 seconds, and remove it at 6 seconds
+  pop.footnote({
+   start: 2,
+   end: 6,
+   text: "Pop!",
+   target: "footnotediv"
+  });
+
+  // play the video right away
+  pop.play();
+}, false);
