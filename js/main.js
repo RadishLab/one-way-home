e72c4050ec6e2c039bf8722c34fb97d5e28d32c5
Edward Wisniewski <edward@radishlab.com>
2014-05-11 10:18:48 -0400

    Updated Map

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 
 var map = new Datamap({
-  scope: 'world',
-  element: document.getElementById('map')
+  element: document.getElementById('map'),
+  scope: 'usa',
+  geographyConfig: {
+    highlightOnHover: false,
+    popupOnHover: false
+  },
+  fills: {
+    defaultFill: 'rgba(211,209,167,0.9)' //any hex, color name or rgb/rgba value
+  }
 });
