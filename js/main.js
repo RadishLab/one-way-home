8745b2161b7de65849f960de1c2ef08e03c7340a
Edward Wisniewski <edward@radishlab.com>
2014-05-11 10:08:10 -0400

    Added js for datamaps

--- js/main.js
+++ js/main.js
@@ -1,50 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Create a popcorn instance by calling Popcorn("#id-of-my-video")
-  var pop = Popcorn("#flag-waving");
 
-  // add a footnote at 2 seconds, and remove it at 6 seconds
-  pop.footnote({
-   start: 2,
-   end: 6,
-   text: "Hello, Darling",
-   target: "caption",
-   effect: "applyclass",
-  });
-  pop.footnote({
-   start: 7,
-   end: 10,
-   text: "What is your name?",
-   target: "caption1",
-   effect: "applyclass",
-  });
-
-
-  // play the video right away
-  pop.autoplay("true");
-}, false);
-
-var myApp = angular.module('myApp',[]);
-myApp.factory('UserService', function() {
-  var userService = {};
-
-  userService.name = "Hello, Darling";
-
-  userService.ChangeName = function (value) {
-    userService.name = value;
-  };
-
-return userService;
+var map = new Datamap({
+  scope: 'world',
+  element: document.getElementById('map')
 });
-
-function MyCtrl($scope, UserService) {
-    $scope.name = UserService.name;
-    $scope.updatedname="";
-    $scope.changeName=function(data){
-        $scope.updateServiceName(data);
-    }
-    $scope.updateServiceName = function(name){
-      UserService.ChangeName(name);
-      $scope.name = UserService.name;
-    }
-}
-
